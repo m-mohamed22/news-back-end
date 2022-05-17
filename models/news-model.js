9115ee8ba1257218a0a663d4ea6dcc34eb809670
m-mohamed22 <mmanwar679@gmail.com>
2022-05-17 14:24:23 +0100

    complete ticket 4

--- models/news-model.js
+++ models/news-model.js
@@ -1,7 +1,14 @@
 const db = require("../db/connection.js");
 
-exports.selectNews = () => {
+exports.selectTopics = () => {
   return db.query("SELECT * FROM topics;").then((topic) => {
     return topic.rows;
   });
+};
+exports.selectArticleById = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .then((articles) => {
+      return articles.rows;
+    });
 };
