a2f6979fc400c57e88db0ffb2b9037eda071943f
m-mohamed22 <mmanwar679@gmail.com>
2022-05-19 16:17:59 +0100

    complete ticket 7 with the comment_count function

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,6 +10,7 @@ afterAll(() => {
   if (db.end) db.end();
 });
 
+/***topics***/
 describe("1. GET/api/topics", () => {
   test("status:200, responds with an array of topics objects", () => {
     return request(app)
@@ -39,6 +40,7 @@ describe("1. GET/api/topics", () => {
   });
 });
 
+/***articles***/
 describe("2. GET/api/articles/:article_id", () => {
   test("Status:200, responds with an article object", () => {
     const article_id = 3;
@@ -59,12 +61,57 @@ describe("2. GET/api/articles/:article_id", () => {
         expect(body.article).toMatchObject(thirdArticle);
       });
   });
+  test("Status:200, responds a selected article that contains comment_count object", () => {
+    const time = "2020-06-06T09:10:00.000Z";
+    const articleID = 9;
+    const ninthArticle = {
+      article_id: 9,
+      title: "They're not exactly dogs, are they?",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "Well? Think about it.",
+      created_at: time,
+      votes: 0,
+      comment_count: 2,
+    };
+    return request(app)
+      .get(`/api/articles/${articleID}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual(ninthArticle);
+        expect(body.article.comment_count).toBe(2);
+      });
+  });
   test("Status:404 returns an error message if article that could exist but does not", () => {
     return request(app)
       .get("/api/articles/888")
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("Invalid ID not found");
+      });
+  });
+});
+
+describe("7.GET /api/articles/:article_id(comment_count)", () => {
+  test("Status:200, responds a selected article, that contains comment_count", () => {
+    const time = "2020-06-06T09:10:00.000Z";
+    const articleID = 9;
+    const ninthArticle = {
+      article_id: 9,
+      title: "They're not exactly dogs, are they?",
+      topic: "mitch",
+      author: "butter_bridge",
+      body: "Well? Think about it.",
+      created_at: time,
+      votes: 0,
+      comment_count: 2,
+    };
+    return request(app)
+      .get(`/api/articles/${articleID}`)
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toEqual(ninthArticle);
+        expect(body.article.comment_count).toBe(2);
       });
   });
 });
@@ -143,6 +190,7 @@ describe("3: PATCH /api/articles/:article_id", () => {
   });
 });
 
+/***users***/
 describe("6. GET/api/users", () => {
   test("status:200, responds with an array of objects with the property username", () => {
     return request(app)
